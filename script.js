9f34472aa22b81c200850e096722097772397921
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-05-18 13:15:57 -0300

    commit atualização
    substituição para laço de repetição

--- script.js
+++ script.js
@@ -2,15 +2,11 @@ function confirmar() {
     var n = document.getElementById('num')
     var num = Number(n.value)
     var msg = document.getElementById('msg')
-    msg.innerHTML = `<h2>A tabuada de ${num} é:</h2>`
-    msg.innerHTML += `<h2>${num}x 1 = ${num * 1}</h2>`
-    msg.innerHTML += `<h2>${num}x 2 = ${num * 2}</h2>`
-    msg.innerHTML += `<h2>${num}x 3 = ${num * 3}</h2>`
-    msg.innerHTML += `<h2>${num}x 4 = ${num * 4}</h2>`
-    msg.innerHTML += `<h2>${num}x 5 = ${num * 5}</h2>`
-    msg.innerHTML += `<h2>${num}x 6 = ${num * 6}</h2>`
-    msg.innerHTML += `<h2>${num}x 7 = ${num * 7}</h2>`
-    msg.innerHTML += `<h2>${num}x 8 = ${num * 8}</h2>`
-    msg.innerHTML += `<h2>${num}x 9 = ${num * 9}</h2>`
-    msg.innerHTML += `<h2>${num}x 10 = ${num * 10}</h2>`
-  }
+    var a = 1
+    msg.innerHTML = `<h1> A tabuada de ${num} é: </h1>`
+    while (a <= 10){
+      msg.innerHTML += (`<h2> ${num} X ${a} = ${num*a} </h2>`)
+      a++
+    } msg.innerHTML += (`Fim da tabuada.` )
+  
+}
